283d277f90edb457924f0e53bde95a2e1fc10423
Dilva Queiroz <dilvaqueiroz@hotmail.com>
2024-07-27 11:55:01 -0300

    ajustes

--- src/layout/HeaderPage.js
+++ src/layout/HeaderPage.js
@@ -1,52 +1,29 @@
-import { Grid, Image, Layout, Menu, Input } from 'antd';
-import { Link, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-import { useTheme } from '../context/ThemeContext';
-import { useState } from 'react';
-import Logo from '../assets/open3.png'
+import { Grid, Image, Layout, Menu, Input, Col, Row } from "antd";
+import { Link, NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { useTheme } from "../context/ThemeContext";
+import { useState } from "react";
+import Logo from "../assets/open3.png";
 
-
-const { Header } = Layout; 
+const { Header } = Layout;
 const { Search } = Input;
 const { useBreakpoint } = Grid;
 
 const StyledHeader = styled(Header)`
     background-color: #2f2e2e;
-`
+`;
 
 const StyledSearch = styled(Search)`
     .ant-input-wrapper .ant-btn {
-        background-color: #FF0000 !important;
-        background: #FF0000 !important;
+        background-color: #ff0000 !important;
+        background: #ff0000 !important;
     }
     .ant-input-wrapper .ant-btn:hover {
         background-color: #f84000 !important;
         background: #f84000 !important;
     }
-`
-
-const StyledMasterContainerHeader = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
 `;
 
-const StyledContainerImg = styled(Link)`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
-const StyledContainerHeader = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
-const MenuContainer = styled.div`
-    gap: 20px;
-
-`;
 
 const StyledMenu = styled(Menu)`
     .ant-menu-title-content::before .active {
@@ -56,7 +33,6 @@ const StyledMenu = styled(Menu)`
 `;
 
 export function HeaderPage() {
-
     const onSearch = (value, _e, info) => console.log(info?.source, value);
     const { toggleTheme, theme } = useTheme();
     const [showMenuVertical, setShowMenuVertical] = useState(false);
@@ -66,10 +42,10 @@ export function HeaderPage() {
     const isMobile = !screens.md;
 
     const list = [
-        { label: 'Inicio', path: '/home', key: "home" },
-        { label: 'Sobre', path: '/about', key: "about" },
-        { label: 'Contato', path: '/contact', key: "contact" },
-    ]
+        { label: "Inicio", path: "/home", key: "home" },
+        { label: "Sobre", path: "/about", key: "about" },
+        { label: "Contato", path: "/contact", key: "contact" },
+    ];
 
     const handleMenuClick = (item) => {
         setShowMenuVertical(false);
@@ -78,29 +54,27 @@ export function HeaderPage() {
 
     return (
         <StyledHeader>
-            <StyledMasterContainerHeader>
-                <StyledContainerImg to="/home" >
-                    <Image src={Logo} width={100} height={64} preview={false} />
-                </StyledContainerImg>
-                <StyledContainerHeader>
-                    <MenuContainer>
-                        <StyledMenu
-                            mode='horizontal'
-                            style={{ width: '15rem' }}
-                            selected={selectedMenuItem}
-                            selectedKeys={[selectedMenuItem && selectedMenuItem.toString()]}
-                            onClick={handleMenuClick}
-                        >
-                            {list.map((item) => (
-                                <Menu.Item key={item.key} style={{ color: 'white' }}>
-                                    <NavLink to={item.path}>{item.label}</NavLink>
-                                </Menu.Item>
-                            ))}
-                        </StyledMenu>
-                    </MenuContainer>
-                    <StyledSearch placeholder="Pesquisar" size='middle' onSearch={onSearch} enterButton />
-                </StyledContainerHeader>
-            </StyledMasterContainerHeader>
+            <Row gutter={24} style={{width: '100%'}}>
+                <Col span={24} flex={1} style={{ display: "flex", alignItems: "center", justifyContent: 'center' }}>
+                    <Link to="/home">
+                        <Image src={Logo} width={100} height={64} preview={false} />
+                    </Link>
+                    <StyledMenu
+                        mode="horizontal"
+                        style={{ width: "15rem" }}
+                        selected={selectedMenuItem}
+                        selectedKeys={[selectedMenuItem && selectedMenuItem.toString()]}
+                        onClick={handleMenuClick}
+                    >
+                        {list.map((item) => (
+                            <Menu.Item key={item.key} style={{ color: "white" }}>
+                                <NavLink to={item.path}>{item.label}</NavLink>
+                            </Menu.Item>
+                        ))}
+                    </StyledMenu>
+                    <StyledSearch placeholder="Pesquisar" size="middle" onSearch={onSearch} enterButton style={{width: isMobile ? '40%' : '15%'}}/>
+                </Col>
+            </Row>
         </StyledHeader>
-    )
+    );
 }
